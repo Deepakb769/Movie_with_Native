4577f9f9c83a8d0f9ded8c7f15b724848624eb90
Deepakb769 <deepakbohora01@gmail.com>
2025-06-12 17:13:54 +0530

    native setup

---
+++ movie-app/app/index.tsx
@@ -0,0 +1,13 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+const index = () => {
+    return(
+        <View className="flex-1 justify-center items-center ">
+            <Text className="text-5xl">Welcome</Text>
+            <Link href='/onboarding'>Onboarding</Link>
+        </View>
+    )
+}
+
+export default index;
